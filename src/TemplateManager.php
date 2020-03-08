04a5a3c14655d0b6ac0cc9fd06696b951205f187
kenni.bertucat <kenni.bertucat@alterway.fr>
2020-03-08 23:38:48 +0100

    [REFACTO #8] Method computeText updated and exceptions catched in getTemplateComputed

--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -21,10 +21,15 @@ class TemplateManager
      */
     public function getTemplateComputed(Template $template, array $data) : Template
     {
-        if (!$template) {
-            throw new \RuntimeException('no tpl given');
+        try {
+            if (!$template) {
+                throw new \RuntimeException('no tpl given');
+            }
+            return $this->computeTexts($template, $data);
+        } catch (RuntimeException $e) {
+            echo "Error in TemplateManager - " . $e->getMessage() . "\n";
+            exit;
         }
-        return $this->computeTexts($template, $data);
     }
 
     /**
@@ -46,56 +51,16 @@ class TemplateManager
     /**
      * @param string $text
      * @param array $data
+     * @return string
      */
     private function computeText(string &$text, array $data)
     {
-        $quote = (isset($data['quote']) and $data['quote'] instanceof Quote) ? $data['quote'] : null;
-
-        if ($quote)
-        {
-            $_quoteFromRepository = QuoteRepository::getInstance()->getById($quote->id);
-            $usefulObject = SiteRepository::getInstance()->getById($quote->siteId);
-            $destinationOfQuote = DestinationRepository::getInstance()->getById($quote->destinationId);
-
-            if(strpos($text, '[quote:destination_link]') !== false){
-                $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-            }
-
-            $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-            $containsSummary     = strpos($text, '[quote:summary]');
-
-            if ($containsSummaryHtml !== false || $containsSummary !== false) {
-                if ($containsSummaryHtml !== false) {
-                    $text = str_replace(
-                        '[quote:summary_html]',
-                        Quote::renderHtml($_quoteFromRepository),
-                        $text
-                    );
-                }
-                if ($containsSummary !== false) {
-                    $text = str_replace(
-                        '[quote:summary]',
-                        Quote::renderText($_quoteFromRepository),
-                        $text
-                    );
-                }
-            }
-
-            (strpos($text, '[quote:destination_name]') !== false) and $text = str_replace('[quote:destination_name]',$destinationOfQuote->countryName,$text);
+        $this->context->update($data);
+        foreach (Token::NAMES as $token) {
+            $tokenClass = Tools::generateClassName($token);
+            $tokenObj = new $tokenClass($token);
+            $text = $tokenObj->replace($text, $this->context);
         }
-
-        if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $usefulObject->url . '/' . $destination->countryName . '/quote/' . $_quoteFromRepository->id, $text);
-        else
-            $text = str_replace('[quote:destination_link]', '', $text);
-
-        /*
-         * USER
-         * [user:*]
-         */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $this->context->getCurrentUser();
-        if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
-        }
+        return $text;
     }
 }
